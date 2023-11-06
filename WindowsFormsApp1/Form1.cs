954675f40ded568159f8586fd474c6c4c87381f8
JiyulLe <148319292+JiyulLe@users.noreply.github.com>
2023-11-06 18:17:53 +0900

    Update Form1.cs

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -21,6 +21,7 @@ namespace WindowsFormsApp1
             textBox_print.Text = "이것은\r\n멀티라인\r\n텍스트박스\r\n인데요.";
             //sdffa
             //sdfjksfj
+            // Edited Github
         }
     }
 }
